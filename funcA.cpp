20ace365b0444abc051be9bcf7a7fb008ee6f59a
Yegor <yegor@Ubuntu.myguest.virtualbox.org>
2024-10-16 19:17:56 +0300

    Updated FuncA

--- funcA.cpp
+++ funcA.cpp
@@ -1,6 +1,15 @@
 #include "funcA.h"
 #include <cmath>
 
+double factorial(int num) {
+	if (num <= 1) return 1;
+	return num * factorial(num - 1);
+}
+
 double FuncA::calculateCosine(double x, int n) {
-	return 0;
+	double result = 0;
+	for (int i = 0; i < n; ++i) {
+		result += pow(-1, i) * pow(x, 2 * i) / factorial(2 * i);
+	}
+	return result;
 }
