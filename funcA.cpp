717e25785fe8764c29d8efa0e40176cc1fdfdae6
Yegor <yegor@Ubuntu.myguest.virtualbox.org>
2024-10-16 19:29:31 +0300

    Hotfix: FuncA now computes sum of first e terms

--- funcA.cpp
+++ funcA.cpp
@@ -1,6 +1,16 @@
 #include "funcA.h"
 #include <cmath>
 
+double factorial(int num) {
+	if (num <= 1) return 1;
+	return num * factorial(num - 1);
+}
+
 double FuncA::calculateCosine(double x, int n) {
-	return 0;
+	int max_terms = 3;
+	double result = 0;
+	for (int i = 0; i < max_terms; ++i) {
+		result += pow(-1, i) * pow(x, 2 * i) / factorial(2 * i);
+	}
+	return result;
 }
