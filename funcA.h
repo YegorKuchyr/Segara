ec870a279e60f77c9785828c900c5280dc875081
Yegor <yegor@Ubuntu.myguest.virtualbox.org>
2024-11-13 21:11:22 +0200

    Add Debian

--- funcA.h
+++ funcA.h
@@ -3,9 +3,12 @@
 
 //FuncA class contains methods to calculate cosine using Taylor series
 //The parameter n indicates how many terms of the series to compute
+
 class FuncA {
 	public:
 		double calculateCosine(double x, int n);
 };
+
+double factorial(int num);
 
 #endif
